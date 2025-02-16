7351bd4de94a3599d933281b7b01fce66b706931
Satish Patil <syapatil0803@gmail.com>
2025-02-16 11:02:18 +0530

    .

---
+++ src/main/java/com/microservice/backand/scrapify_portal/ScrapifyPortalApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.backand.scrapify_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapifyPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapifyPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/backand/scrapify_portal/ScrapifyPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.backand.scrapify_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapifyPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
